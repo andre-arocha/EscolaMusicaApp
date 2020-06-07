6ab938ffb29f637aa24b1011b1673793c27a8d2f
Andre Rocha <andre.a.rocha2018@hotmail.com>
2020-06-07 13:00:56 -0300

    Finalizando curso

--- WindowsFormsApp1/Banco.cs
+++ WindowsFormsApp1/Banco.cs
@@ -10,7 +10,7 @@ namespace EscolarMusicApp
     public static class Banco
     {
         public static string StrCon =
-            @"server=ca109-pl.valueserver.net;user id=aluno_itoo;database=wellington_88db;password=79Tgdo@5";
+            @"server=***;user id=***;database=***;password=***";
         public static MySqlCommand AbriConexao()
         {
             MySqlCommand cmd = new MySqlCommand();
